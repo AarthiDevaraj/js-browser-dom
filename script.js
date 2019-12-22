d944e00fa6351decfa1eec372cf8819be2a94740
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-22 22:05:50 +0530

    done

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ xhr.open('POST', 'https://jsonplaceholder.typicode.com/todos');
 xhr.setRequestHeader('X-Auth-A', 'Auth A Value');
 xhr.setRequestHeader('X-Auth-B', 'Auth B Value');
 xhr.setRequestHeader('X-Custom-A', 'Custom A Value');
+xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8'); // for json payload
 xhr.send(data);
 
 xhr.onload = function() {
