ae5752dacb6d785431edf788c6736d211835bd49
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-22 21:58:37 +0530

    done

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-console.log(window.navigator);
+var xhr = new XMLHttpRequest();
 
-//  Please take a look here: https://developer.mozilla.org/en-US/docs/Web/API/NavigatorID/appName
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+
+xhr.send();
+
+xhr.onload = function() {
+  if (xhr.status != 200) {
+    console.log('Ajax call failed', xhr.statusText);
+  } else {
+    console.log('Ajax request successful', xhr.response);
+  }
+};
+
+xhr.onerror = function() {
+  console.log('Request failed');
+};
