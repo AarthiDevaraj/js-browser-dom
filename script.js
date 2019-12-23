2b0078fa2e1429a7f254399723a3e1795f095324
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-23 15:07:41 +0530

    done

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//  get reference to table
+var tableRefCollection = document.getElementsByTagName('table'); //  collection
+var tableRef = tableRefCollection[0];
+
+var tableBodyRef = tableRef.lastElementChild;
+
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
@@ -9,11 +15,50 @@ xhr.onload = function() {
     console.log('Ajax call failed', xhr.statusText);
   } else {
     var data = JSON.parse(xhr.response); //  response is string. parse it to json object
-    console.log('Ajax request successful', data);
-    document.getElementById('dump').textContent = JSON.stringify(data); //  stringify converts object to string
+    loadTable(data);
   }
 };
 
 xhr.onerror = function() {
   console.log('Request failed');
 };
+
+function loadTable(data) {
+  for (let index = 0; index < data.length; index++) {
+    var trElement = createTableRow(data[index]);
+    tableBodyRef.appendChild(trElement);
+  }
+}
+
+function createTableRow(line) {
+  //  create tr element
+  var newTr = document.createElement('tr');
+
+  //  append tds to tr
+  newTr.appendChild(createTableCell(line.id));
+  newTr.appendChild(createTableCell(line.title));
+
+  //  customizing the text for completed property
+  if (line.completed) {
+    newTr.appendChild(createTableCell('Yes'));
+  } else {
+    newTr.appendChild(createTableCell('No'));
+  }
+
+  //  return4
+  return newTr;
+}
+
+function createTableCell(property) {
+  //  create a new td element
+  var newTd = document.createElement('td');
+
+  //  create a new text node
+  var newTextNode = document.createTextNode(property);
+
+  //  append text node to newTd
+  newTd.appendChild(newTextNode);
+
+  //  return
+  return newTd;
+}
